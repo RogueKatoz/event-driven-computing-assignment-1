7a3d5c4717f693b4162414126bf06d764418104e
RogueKatoz <adam.katic1@gmail.com>
2026-09-05 04:46:00 +0000

    feat: wrap bufferedReader in try block, read first input line and store as regex, check for verbose mode, then print ready

--- RegexEngine.java
+++ RegexEngine.java
@@ -1,20 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.Arrays;
+
 
 public class RegexEngine {
 
+    public static boolean debug;
+
+    public static void printTransitionTable(String regex) {
+        // print the transition table corresponding to the regex string
+        // only if verbose mode is enabled
+        if (debug == true) System.out.println("DEBUG: Printing transition table for regex: " + regex);
+        return;
+    }
     public static void main(String[] args) {
         
         // sets debug mode
-        boolean debug = Arrays.stream(args)
+        debug = Arrays.stream(args)
             .anyMatch(arg -> arg.equals("-d"));
 
         // sets verbose mode
         boolean verbose = Arrays.stream(args)
             .anyMatch(arg -> arg.equals("-v"));
-        if (debug == true) System.out.println("Verbose mode: " + verbose);
+        if (debug == true) System.out.println("DEBUG: Verbose mode: " + verbose);
 
     
-        //todo: parse the first input line and store as regex
+        // parse the first input line and store as regex
+        try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in))) {
+            if (debug == true) System.out.println("DEBUG: BufferedReader created");
+            String regex = inputReader.readLine();
+            
+            if (verbose == true) printTransitionTable(regex);
+            System.out.println("ready");
+
+
+        } catch (IOException e) {
+            System.out.println("An error has occured:" + e.getMessage());
+        }
+
 
         //todo: parse/evaluate subsequent lines
 
