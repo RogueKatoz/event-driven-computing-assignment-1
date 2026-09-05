1f271bc5e6203acbec37b7b67414e6610b315a98
RogueKatoz <adam.katic1@gmail.com>
2026-09-05 04:56:40 +0000

    feat: parse subsequent input strings and send to comparison function. Print false by default for now

--- RegexEngine.java
+++ RegexEngine.java
@@ -14,6 +14,13 @@ public class RegexEngine {
         if (debug == true) System.out.println("DEBUG: Printing transition table for regex: " + regex);
         return;
     }
+
+    public static boolean evaluateInputString(String regex, String inputString) {
+        // takes a regular expression and a second input string to compare against it
+        // prints
+        if (debug == true) System.out.println("DEBUG: Comparing string: " + inputString + " against regular expression " + regex);
+        return false;
+    }
     public static void main(String[] args) {
         
         // sets debug mode
@@ -25,22 +32,23 @@ public class RegexEngine {
             .anyMatch(arg -> arg.equals("-v"));
         if (debug == true) System.out.println("DEBUG: Verbose mode: " + verbose);
 
-    
-        // parse the first input line and store as regex
-        try (BufferedReader inputReader = new BufferedReader(new InputStreamReader(System.in))) {
+        // parse the first input line, store as regex, then print transition table if verbose
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
             if (debug == true) System.out.println("DEBUG: BufferedReader created");
-            String regex = inputReader.readLine();
+            String regex = reader.readLine();
             
             if (verbose == true) printTransitionTable(regex);
             System.out.println("ready");
 
+            // Parse/evaluate subsequent lines
+            String inputString;
+            while ((inputString = reader.readLine()) != null) {
+                System.out.println(evaluateInputString(regex, inputString));
+            }
 
         } catch (IOException e) {
             System.out.println("An error has occured:" + e.getMessage());
         }
-
-
-        //todo: parse/evaluate subsequent lines
 
     }
 }
